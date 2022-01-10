5eef58ee2d727c019396809679df2daa6b468504
Jonas Urth Olsen <ojoo@dr.dk>
2022-01-10 10:36:33 +0100

    Codestyle fixes

--- frontend/components/nav/nav.js
+++ frontend/components/nav/nav.js
@@ -2,21 +2,25 @@ import { React, useEffect } from "react"
 import { useContext } from "react"
 import { GlobalContext } from "../../pages/_app"
 import Link from "next/link"
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
-import styles from './nav.module.scss'
-
+import UIkit from "uikit"
+import Icons from "uikit/dist/js/uikit-icons"
+import styles from "./nav.module.scss"
 
 const Nav = ({ categories }) => {
   useEffect(() => {
     UIkit.use(Icons)
-  });
+  })
 
   const { siteName } = useContext(GlobalContext)
   return (
     <div>
-      <nav className={`uk-navbar-container uk-navbar ${styles.container}`} data-uk-navbar>
-        <a class="uk-navbar-item uk-logo" href="/">{siteName}</a>
+      <nav
+        className={`uk-navbar-container uk-navbar ${styles.container}`}
+        data-uk-navbar
+      >
+        <a className="uk-navbar-item uk-logo" href="/">
+          {siteName}
+        </a>
 
         <div className="uk-navbar-left">
           <ul className="uk-navbar-nav">
@@ -32,10 +36,14 @@ const Nav = ({ categories }) => {
           </ul>
         </div>
 
-
-         <div className="uk-navbar-right">
+        <div className="uk-navbar-right">
           <ul className="uk-navbar-nav uk-iconnav">
-            <li><a href="https://www.instagram.com/afvejen/" uk-icon="icon: instagram"></a></li>  
+            <li>
+              <a
+                href="https://www.instagram.com/afvejen/"
+                uk-icon="icon: instagram"
+              ></a>
+            </li>
           </ul>
         </div>
       </nav>
